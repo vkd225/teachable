b4ed0ad6615299790ec01f272c9a938750d73c7a
vkd225 <vkd225@nyu.edu>
2020-02-08 17:01:58 -0500

    adding apis

---
+++ lambda/lambda_function.py
@@ -0,0 +1,97 @@
+from todowrapper import TODOABLE
+import json
+import logging
+
+logger = logging.getLogger()
+logging.basicConfig()
+logger.setLevel(logging.INFO)
+
+def login(event):
+    user = event.get('username', None)
+    pwd = event.get('password', None)
+    response = TODOABLE.login(user, pwd)
+    return (response)
+
+def get_lists(event):
+    token = event.get('token', None)
+    response = TODOABLE.get_lists(token)
+    return (response)
+
+def post_list(event):
+    token = event.get('token', None)
+    name = event.get('name', None)
+    response = TODOABLE.post_list(token, name)
+    return (response)
+
+def get_list_tasks(event):
+    token = event.get('token', None)
+    list_id = event.get('list_id', None)
+    response = TODOABLE.post_list(token, list_id)
+    return (response)
+
+def update_list(event):
+    token = event.get('token', None)
+    list_id = event.get('list_id', None)
+    name = event.get('name', None)
+    response = TODOABLE.update_list(token, list_id, name)
+    return (response)
+
+def delete_list(event):
+    token = event.get('token', None)
+    list_id = event.get('list_id', None)
+    response = TODOABLE.delete_list(token, list_id)
+    return (response)
+
+def post_task_in_list(event):
+    token = event.get('token', None)
+    list_id = event.get('list_id', None)
+    name = event.get('name', None)
+    response = TODOABLE.post_task_in_list(token, list_id, name)
+    return (response)
+
+def complete_task_in_list(event):
+    token = event.get('token', None)
+    list_id = event.get('list_id', None)
+    item_id = event.get('item_id', None)
+    response = TODOABLE.complete_task_in_list(token, list_id, item_id)
+    return (response)
+
+def delete_task_in_list(event):
+    token = event.get('token', None)
+    list_id = event.get('list_id', None)
+    item_id = event.get('item_id', None)
+    response = TODOABLE.delete_task_in_list(token, list_id, item_id)
+    return (response)
+
+def lambda_handler(event, context):
+    '''
+    Lambda function triggered with correct function name and parameters.
+    '''
+    dispatcher={
+        'login':login,
+        'get_lists':get_lists,
+        'post_list':post_list,
+        'get_list_tasks':get_list_tasks,
+        'update_list':update_list,
+        'delete_list':delete_list,
+        'post_task_in_list':post_task_in_list,
+        'complete_task_in_list':complete_task_in_list,
+        'delete_task_in_list':delete_task_in_list
+    }
+
+    function_name = event.get('function')
+    try:
+        response = dispatcher[function_name](event)
+    except KeyError:
+        raise ValueError('invalid input')
+
+    return response
+
+
+# Entry point for testing
+if __name__ == '__main__':
+    with open('test_event.json', 'r') as f_in:
+        test_event = json.load(f_in)
+
+    response = lambda_handler(test_event,'')
+    logger.info(response)
---
+++ lambda/todowrapper/todoable.py
@@ -0,0 +1,235 @@
+from __future__ import print_function
+import requests
+import json
+
+base_url = 'https://todoable.teachable.tech/api'
+
+class TODOABLE(object):
+    def __init__(self):
+        pass
+
+    def login(username, password):
+        
+        '''
+        Login into todoable. 
+        usage: todoable.login(username, password)
+        '''
+
+        headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+        url = base_url + '/authenticate'
+        response = requests.post(url=url, headers=headers, auth=(username, password))
+        if response.status_code == 200:
+            return (response.json())
+        else:
+            return ('invalid credentials')
+
+
+    def get_lists(token):
+        
+        '''
+        Get Todoable lists. 
+        usage: todoable.get_lists(token)
+        '''
+
+        headers = {
+                'Content-Type': 'application/json', 
+                'Accept': 'application/json',
+                'Authorization': 'Token token={}'.format(token)
+                }
+        url = base_url + '/lists'
+        response = requests.get(url=url, headers=headers)
+
+        if response.status_code == 200:
+            return (response.json())
+        else:
+            return ('invalid token')
+
+
+    def post_list(token, name):
+        
+        '''
+        Post a Todoable list. 
+        usage: todoable.post_lists(token, name)
+        '''
+
+        headers = {
+            'Content-Type': 'application/json', 
+            'Accept': 'application/json',
+            'Authorization': 'Token token={}'.format(token)
+            }
+        url = base_url + '/lists'
+
+        data = {
+            "list": {
+                "name": name
+            }
+        }
+        response = requests.post(url=url, headers=headers, json=data)
+
+        if response.status_code == 201:
+            return (response.json())
+        elif response.status_code == 401:
+            return ('invalid token')
+        else:
+            return ('list name already used')
+
+
+    def get_list_tasks(token, list_id):
+        
+        '''
+        get a Todoable list items. 
+        usage: todoable.get_list_items(token, list_id)
+        '''
+
+        headers = {
+            'Content-Type': 'application/json', 
+            'Accept': 'application/json',
+            'Authorization': 'Token token={}'.format(token)
+            }
+        url = base_url + '/lists/' + list_id
+
+        response = requests.get(url=url, headers=headers)
+
+        if response.status_code == 200:
+            return (response.json())
+        elif response.status_code == 401:
+            return ('invalid token')
+        else:
+            return ('invalid list id')
+
+
+    def update_list(token, list_id, name):
+        
+        '''
+        Updates a Todoable list. 
+        usage: todoable.update_list(token, list_id, new_name)
+        '''
+
+        headers = {
+            'Content-Type': 'application/json', 
+            'Accept': 'application/json',
+            'Authorization': 'Token token={}'.format(token)
+            }
+        url = base_url + '/lists/' + list_id
+        data = {
+            "list": {
+                "name": name
+            }
+        }
+
+        response = requests.patch(url=url, headers=headers, json=data)
+
+        if response.status_code == 200:
+            return ('list updated')
+        elif response.status_code == 401:
+            return ('invalid token')
+        else:
+            return ('invalid list id')
+
+
+    def delete_list(token, list_id):
+        
+        '''
+        Deletes a Todoable list. 
+        usage: todoable.delete_list(token, list_id)
+        '''
+
+        headers = {
+            'Content-Type': 'application/json', 
+            'Accept': 'application/json',
+            'Authorization': 'Token token={}'.format(token)
+            }
+        url = base_url + '/lists/' + list_id
+        
+        response = requests.delete(url=url, headers=headers)
+
+        if response.status_code == 204:
+            return ('list deleted')
+        elif response.status_code == 401:
+            return ('invalid token')
+        else:
+            return ('invalid list id')
+
+
+    def post_task_in_list(token, list_id, name):
+        
+        '''
+        Post an item inside a Todoable list. 
+        usage: todoable.post_task_in_list(token, list_id, task_name)
+        '''
+
+        headers = {
+            'Content-Type': 'application/json', 
+            'Accept': 'application/json',
+            'Authorization': 'Token token={}'.format(token)
+            }
+        url = base_url + '/lists/' + list_id + '/items'
+
+        data = {
+            "item": {
+                "name": name
+            }
+        }
+        response = requests.post(url=url, headers=headers, json=data)
+
+        if response.status_code == 201:
+            return (response.json())
+        elif response.status_code == 401:
+            return ('invalid token')
+        else:
+            return ('invalid input')
+
+
+    def complete_task_in_list(token, list_id, item_id):
+        
+        '''
+        Mark an item inside a Todoable list as done. 
+        usage: todoable.post_lists(token, list_id, task_id)
+        '''
+
+        headers = {
+            'Content-Type': 'application/json', 
+            'Accept': 'application/json',
+            'Authorization': 'Token token={}'.format(token)
+            }
+        url = base_url + '/lists/' + list_id + '/items/' + item_id +'/finish'
+
+        response = requests.put(url=url, headers=headers)
+
+        if response.status_code == 200:
+            return ('marked finished')
+        elif response.status_code == 401:
+            return ('invalid token')
+        else:
+            return ('Already marked as done')
+
+    def delete_task_in_list(token, list_id, item_id):
+        
+        '''
+        Delete an item inside a Todoable list. 
+        usage: todoable.post_lists(token, list_id, task_id)
+        '''
+
+        headers = {
+            'Content-Type': 'application/json', 
+            'Accept': 'application/json',
+            'Authorization': 'Token token={}'.format(token)
+            }
+        url = base_url + '/lists/' + list_id + '/items/' + item_id
+
+        response = requests.delete(url=url, headers=headers)
+
+        print ('response: ', response)
+
+        if response.status_code == 204:
+            return ('item deleted')
+        elif response.status_code == 401:
+            return ('invalid token')
+        else:
+            return ('Already marked as done')
+
+
+
+
+
+
